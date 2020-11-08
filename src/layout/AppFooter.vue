d8ddf5b00e3269833ad4a5234f507e7c1d31d36a
Andy Wang <andynmnwang@gmail.com>
2020-11-07 20:32:15 -0600

    footer embellishments

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -1,9 +1,11 @@
 <template>
-    <footer class="footer text-white bg-dark py-3">
-        <div class="container">
+    <footer class="footer text-white bg-secondary py-3">
+        <div class="container text-dark">
             <div class="row align-items-center">
                 <div class="col-md-1 text-center">
-                    <img src="/img/logo/crest.png" class="img-fluid" />
+                    <router-link to="/">
+                        <img src="/img/logo/crest.png" class="img-fluid" />
+                    </router-link>
                 </div>
                 <div class="col-md-3 text-left my-1">
                     <div class="copyright">
@@ -13,7 +15,8 @@
                 </div>
                 <div class="col-md-8 text-right my-1">
                     <div class="self-promotion">
-                        <a href="https://github.com/awangg" class="text-white" target="_blank" rel="noopener">&lt;/&gt; with &lt;3 by Andy Wang</a>
+                        <a href="https://github.com/awangg" class="text-dark" target="_blank" rel="noopener">&lt;/&gt; with &lt;3 by Andy Wang</a><br/>
+                        <a href="https://github.com/riceduncan/duncan-website/issues" class="text-dark" target="_blank" rel="noopener">Report an Issue</a>
                     </div>
                 </div>
             </div>
