604598d9d9dc6a4f86a34f64c51f5c9ac7100a0e
Andy Wang <andynmnwang@gmail.com>
2020-11-29 18:14:54 -0600

    add duncsquad emails

--- src/views/components/SquadCard.vue
+++ src/views/components/SquadCard.vue
@@ -3,10 +3,11 @@
         <img :src="image" class="img-fluid mb-4" />
         <p class="description text-dark">{{bio}}</p>
         <hr />
-        <div class="row justify-content-center">
+        <div class="row justify-content-center align-items-center">
             <div class="col-lg-4 col-md-12 col-sm-12 my-3" v-for="(member, index) in members" :key="index">
                 <card class="border-0 text-center" :class="{'bg-success': index <= 1}" hover shadow>
-                    <h6 class="font-weight-bold text-uppercase mt-3" :class="{'text-white': index <= 1}">{{member}}</h6>
+                    <h6 class="font-weight-bold text-uppercase mt-3" :class="{'text-white': index <= 1}">{{member.split('|')[0]}}</h6>
+                    <p class="description smaller text-white">{{member.split('|')[1]}}</p>
                 </card>
             </div>
         </div>
